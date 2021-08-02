9430a48e53e709cc487b99783c05c73d9d1d98d6
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2021-08-02 17:24:39 -0500

    Made docker simpler

--- Ubuntu_PopOS.sh
+++ Ubuntu_PopOS.sh
@@ -6,13 +6,7 @@ sudo apt -y install python3 npm
 sudo apt -y install snapd
 sudo apt -y install openjdk-8-jre
 sudo apt -y install lm-sensors
-sudo apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo apt -y install docker.io
 sudo groupadd docker
 sudo usermod -aG docker kpwenzel
 sudo apt -y install qemu
